6a694d22b9d2eee6eda8308d83682aecc8c42947
Toivo Henningsson <toivo.h.h@gmail.com>
2023-11-02 19:54:15 +0100

    Start on python test to compare behavior to C model

--- src/test.py
+++ src/test.py
@@ -2,9 +2,12 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
+waveform_test = True
+compare_test = True
 
 @cocotb.test()
-async def test(dut):
+async def test_waveform(dut):
+	if not waveform_test: return
 	dut._log.info("start")
 	clock = Clock(dut.clk, 2, units="us")
 	cocotb.start_soon(clock.start())
@@ -54,3 +57,85 @@ async def test(dut):
 			file.write("]")
 	else:
 		ClockCycles(dut.clk, 2*period*8)
+
+NUM_OSCS = 2
+NUM_MODS = 3
+
+def sample(v, x): v.append(int(x))
+
+def sample_voice(v, voice):
+	sample(v, voice.y_out.value)
+	for i in range(NUM_OSCS):
+		sample(v, voice.cfg[i].value)
+		sample(v, voice.saw_counter_state[i].value)
+	for i in range(NUM_MODS):
+		sample(v, voice.cfg[i + NUM_OSCS].value)
+		sample(v, voice.mod_counter_state[i].value)
+
+@cocotb.test()
+async def test_compare(dut):
+	if not waveform_test: return
+	dut._log.info("start")
+	clock = Clock(dut.clk, 2, units="us")
+	cocotb.start_soon(clock.start())
+
+	# reset
+	dut._log.info("reset")
+	dut.rst_n.value = 0
+	dut.ui_in.value = 0
+	dut.uio_in.value = 0
+	await ClockCycles(dut.clk, 10)
+	dut.rst_n.value = 1
+
+	# enable
+	dut.ena.value = 1
+
+	preserved = True
+	#preserved = False
+	try:
+		oct_counter = dut.dut.oct_counter.value
+	except AttributeError:
+		preserved = False
+
+	if not preserved: return
+
+	states = dict()
+
+	try:
+		with open("model.data") as f:
+			names = f.readline().split(" ")
+			rev_names = {name: i for (i, name) in enumerate(names)}
+
+			ignore = set()
+			ignore.add(rev_names["out"])
+
+			await ClockCycles(dut.clk, 1)
+			line_number = 0
+			while True:
+				line_number += 1
+				line = f.readline()
+				if line == "": break
+				items = line.split(" ")
+				if line[0] == "c":
+					for item in items[1:]:
+						key, value = item.split(":")
+						states[int(key)] = int(value)
+
+					v = []
+					sample_voice(v, dut.dut)
+
+					match = True
+					for (i, value) in enumerate(v):
+						if not i in ignore and value != states[i]: match = False
+
+					if not match:
+						print()
+						print("Mismatch on line", line_number)
+						for (i, value) in enumerate(v):
+							print(value == states[i], "\t", names[i], ":\t", states[i], ",\t", value)
+
+					assert match
+
+					await ClockCycles(dut.clk, 1)
+	except FileNotFoundError:
+		println("model.data not found!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
